629a6c0d3e1da2bec914c9b113b3e64c2fed5d1d
AILalev22 <ailalev22@codingburgas.bg>
2026-04-03 12:08:02 +0300

    Add initial project

---
+++ FitnessTracker.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessTracker.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services) 
+        {
+            return services;
+        }
+    }
+}
